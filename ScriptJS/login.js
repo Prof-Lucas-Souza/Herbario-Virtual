96f9c988d7a8166e05ef50cf03d8808b610bab91
Prof-Lucas-Souza <lucas.souza@bpkedu.com.br>
2026-09-21 12:41:57 -0300

    Update login.js

--- ScriptJS/login.js
+++ ScriptJS/login.js
@@ -3,8 +3,6 @@
    SISTEMA DE LOGIN
 ========================================= */
 
-
-
 // URL DO WEB APP DO GOOGLE APPS SCRIPT
 const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbytxMrzTl5QdungjAlfKt6FQ5lAmcaGlmAzzRiiAU5j25srhW-0QwZXIOgdKvcmJVi7/exec";
 
